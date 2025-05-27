3d2ed888911cbf8057afccfb74cd124c761fe6cb
jrmyrbt <jeremy.ribot@outlook.com>
2025-05-27 17:24:52 +0200

    creation titre et début de la page analyse

--- application/page-analyse.py
+++ application/page-analyse.py
@@ -1,3 +1,16 @@
 import streamlit as st
-st.title("Page d'analyse")
-st.write("Cette page est dédiée à l'analyse des données et des résultats.")
+import pandas as pd
+
+import streamlit as st
+
+
+st.image("image_data_analyse.png", width=150)
+
+st.title("Analyse des données")
+st.write("Après notre étude de marché sur la consommation de cinéma dans la région de la Creuse, nous avons réalisé une analyse approndie de la base de données pour identifier les tendances et les préférences des spectateurs. ")
+st.write("Ci-dessous, les étapes suivies.")
+
+st.subheader("Pré-filtrage des données")
+st.write("Avant de faire l'analyse, nous avons pré-filtré les données pour réduire notre jeu de données à ~14000 lignes :")
+st.write("- Suppression des genres : Documentary / Short / News / Talk-show / Game-Show / Reality-TV / Adult,")
+st.write("- Affichage des films avec avec au minimum une productrion française, américaine ou britannique,")
